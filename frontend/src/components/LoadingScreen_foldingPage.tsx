50058529babb88901f9d8cf56b4c7ac2a0d3d28e
RechadSalma <rechadsalma123@gmail.com>
2022-07-08 09:44:59 +0100

    change the loading screen, removed the h2 & tweaked the background gradient colour

--- frontend/src/components/LoadingScreen_foldingPage.tsx
+++ frontend/src/components/LoadingScreen_foldingPage.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
 import "../styles/components/LoadingScreen_foldingPage.scss";
+// import Image_responsive from "./Image_responsive";
+// import bugTrackerImg575 from "../public/images/Bug-Tracker-575.jpg";
 
 type Props = {};
 
 export default function LoadingScreen_foldingPage({}: Props) {
   return (
     <div id="LoadingScreen_foldingPage-views">
-      <Spinner animation="border" variant="secondary" />
-      <h2>Bug Tracker loading</h2>
+      {/* <h2>Bug Tracker loading</h2> */}
+      {/* <div className="logoimgLoadingPage">
+        <Image_responsive
+          imageSrc={bugTrackerImg575}
+          altString="bug tracker loading screen"
+        />
+      </div> */}
+
+      {/* <br /> */}
+      <Spinner
+        className={`spinnerLoadingPage`}
+        animation="border"
+        variant="primary"
+      />
     </div>
   );
 }
