7a3d726925b349907ef88dff21edfc3f6e2c175b
ilshaad <82680145+ilshaad@users.noreply.github.com>
2025-06-12 21:02:30 +0100

    included heroku sh commands to deploy

--- backend/herokudeploy.sh
+++ backend/herokudeploy.sh
@@ -1,7 +1,9 @@
 docker build -t ikbugtrackerbackend2025 . && \
 docker login && \
-docker tag ikbugtrackerbackend2025:latest rechadkheerdali/ikbugtrackerbackend2025 && \
-docker push rechadkheerdali/ikbugtrackerbackend2025 && \
-heroku container: login && \
-heroku create -a ikbugtrackerbackend2025app && \
-heroku container:push web -a 
+docker tag ikbugtrackerbackend2025:latest rechadsalma/ikbugtrackerbackend2025 && \
+docker push rechadsalma/ikbugtrackerbackend2025 && \
+heroku container:login && \
+heroku create -a bug-tracker-backend-ik-202203 && \
+heroku container:push web -a bug-tracker-backend-ik-202203 && \
+heroku container:release web -a bug-tracker-backend-ik-202203 
+
---
+++ frontend/herokudeploy.sh
@@ -0,0 +1,9 @@
+docker build -t ikbugtrackerfrontend2025 . && \
+docker login && \
+docker tag ikbugtrackerfrontend2025:latest rechadsalma/ikbugtrackerfrontend2025  && \
+docker push rechadsalma/ikbugtrackerfrontend2025  && \
+heroku container:login && \
+heroku create -a bug-tracker-frontend-ik-202203 && \
+heroku container:push web -a bug-tracker-frontend-ik-202203 && \
+heroku container:release web -a bug-tracker-frontend-ik-202203 
+
