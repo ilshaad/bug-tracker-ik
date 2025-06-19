1945c122693f5da142f9b4a16c4b379f0220d061
ilshaad <82680145+ilshaad@users.noreply.github.com>
2025-06-19 19:06:36 +0100

    switch dockerfile & dockerfile.dev

--- frontend/herokudeploy.sh
+++ frontend/herokudeploy.sh
@@ -5,5 +5,14 @@ docker push rechadsalma/ikbugtrackerfrontend2025  && \
 heroku container:login && \
 heroku create -a bug-tracker-frontend-ik-202203 && \
 heroku container:push web -a bug-tracker-frontend-ik-202203 && \
-heroku container:release web -a bug-tracker-frontend-ik-202203 
+heroku container:release web -a bug-tracker-frontend-ik-202203
+
+# docker build -t ikbugtrackerfrontend2025 -f Dockerfile.dev . && \
+# docker login && \
+# docker tag ikbugtrackerfrontend2025:latest rechadsalma/ikbugtrackerfrontend2025  && \
+# docker push rechadsalma/ikbugtrackerfrontend2025  && \
+# heroku container:login && \
+# heroku create -a bug-tracker-frontend-ik-202203 && \
+# heroku container:push web -a bug-tracker-frontend-ik-202203 && \
+# heroku container:release web -a bug-tracker-frontend-ik-202203 
 
